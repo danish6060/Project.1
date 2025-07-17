2ac0745bb667677cd46e76588984148b7deb2925
Muhammad Danish <danishbaloch6060@gmail.com>
2025-07-17 10:05:25 +0500

    Add files via upload

---
+++ Website Java.js
@@ -0,0 +1,10 @@
+document.body.style.background="Pink"
+
+let elements=document.querySelectorAll("p")
+console.log(elements)
+
+let firstClass=document.querySelectorAll(".Heading-Class")
+console.log(firstClass)
+
+let firstclass=document.querySelector(".Heading-Class")
+console.log(firstClass) 
